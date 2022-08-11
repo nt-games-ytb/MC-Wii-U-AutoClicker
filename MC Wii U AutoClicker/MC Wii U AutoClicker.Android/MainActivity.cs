d33e8b28025a9ddcaaa0c32bfe4f9a9825f9e431
Nicolas TORO <toronico07@outlook.fr>
2022-08-11 20:54:22 +0200

    Update app design

--- MC Wii U AutoClicker/MC Wii U AutoClicker.Android/MainActivity.cs
+++ MC Wii U AutoClicker/MC Wii U AutoClicker.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace MC_Wii_U_AutoClicker.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 178, 34, 34));//Set notification bar in FireBrick color #FFB22222
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
